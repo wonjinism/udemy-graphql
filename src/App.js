fc74ac73b04cc331b38c314ea67e689295613eba
Wonjin Kim <wonjinism@gmail.com>
2021-01-06 15:24:30 +0900

    show search result

--- src/App.js
+++ src/App.js
@@ -49,9 +49,24 @@ class App extends Component {
               const repositoryCount = search.repositoryCount
               const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories' 
               const title = `Github Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
-              return <div>
-                <h1>{title}</h1>
-              </div>
+              return (
+              <React.Fragment>
+                <h2>{title}</h2>
+                <ul>
+                  { 
+                    search.edges.map(edge => {
+                      const node = edge.node
+                      return (
+                        <li key={node.id}>
+                          <a href={node.url} target="_blank">{node.name}</a>
+                        </li>
+
+                      )
+                    })
+                  }
+                </ul>
+              </React.Fragment>
+              )
             }
           }
         </Query>
